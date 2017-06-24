a45b05a85ab66a4688cae875a212893fbc8a9c8c
janjira <janjira.eriksson@gmail.com>
2017-06-24 22:54:07 +0200

    Added button and footer into the GUI

--- CpGui/src/main/java/com/mobilityguard/acc/CpGui/CpUI.java
+++ CpGui/src/main/java/com/mobilityguard/acc/CpGui/CpUI.java
@@ -6,6 +6,7 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
@@ -35,10 +36,9 @@ public class CpUI extends UI {
     	setContent(root);
 
     	final HorizontalLayout titleBar = new HorizontalLayout();
-    	root.setStyleName("titleBar");
+    	titleBar.setStyleName("titleBar");
     	titleBar.setWidth("100%");
-    	root.addComponent(titleBar);
-    	
+    	root.addComponent(titleBar);   	
     	final Label titleLabel = new Label("Control Panel");
     	titleLabel.addStyleName("titleLabel");
     	titleLabel.setWidth("100%");
@@ -63,9 +63,9 @@ public class CpUI extends UI {
     	selectMenu.setWidth("100%");
     	selectBackground.addComponent(selectMenu);
     	
-    	final GridLayout grid = new GridLayout(4,6);
+    	final GridLayout grid = new GridLayout(6,6);
     	grid.addStyleName("grid");
-    	grid.setWidth("600px");
+    	grid.setWidth("1000px");
     	grid.setHeight("500px");
     	
     	grid.addComponent(new Label("a"),1,0);
@@ -83,76 +83,22 @@ public class CpUI extends UI {
     	grid.addComponent(new Label("6"),2,5);
 
     	selectBackground.addComponent(grid);
+    	final HorizontalLayout buttonBackground= new HorizontalLayout();
+    	buttonBackground.setStyleName("buttonBackground");    	
+    	final Button bOk = new Button("ok");
+    	final Button bCancel = new Button("cancel");  	
+    	buttonBackground.addComponents(bOk,bCancel);
+    	root.addComponent(buttonBackground);
+    	root.setComponentAlignment(buttonBackground, Alignment.BOTTOM_RIGHT);
     	
-//    	 grid.addComponent(titleText, 2,0);
-//         grid.addComponent(descriptionText,1,2);
-//         grid.addComponent(descriptionValue,2,2);
-//         grid.addComponent(validTimeText,1,3);
-//         grid.addComponent(timeSelectCombobox,2,3);
-//         grid.addComponent(certProfileText,1,4);
-//         grid.addComponent(profileSelectCombobox,2,4);
-//         grid.addComponent(providerText,1,5);
-//         grid.addComponent(providerCombobox,2,5);
-//         grid.addComponent(userSelectText,1,6);
-//         grid.addComponent(userSelectedValue,2,6);
-//         grid.addComponent(okButton,2,7);
-//         grid.addComponent(cancelButton,3,7);
-//         grid.setComponentAlignment(okButton, Alignment.TOP_RIGHT);
-//         grid.setComponentAlignment(cancelButton, Alignment.TOP_LEFT);
-//         grid.setMargin(true);
-//         grid.setSpacing(true);
-    	
-//    	final HorizontalLayout HoriBackgroundLeft = new HorizontalLayout();
-//    	HoriBackgroundLeft.setSpacing(true);
-//    	final VerticalLayout vartiInside = new VerticalLayout();
-//    	vartiInside.setWidth("80%");
-//    	vartiInside.setSpacing(true);
-//    	vartiInside.setMargin(true);
-//    	final Label label1 = new Label("text 1");
-//    	label1.setStyleName("label1");
-//    	label1.setHeight("30%");
-//    	final Label label2 = new Label("text 2");
-//    	final Label label3 = new Label("text 3");
-//    	final Label label4 = new Label("text 4");
-//    	final Label label5 = new Label("text 5");
-//    	final Label label6 = new Label("text 6");
-//    	final Label label7 = new Label("");
-//    	final Label label8 = new Label(" ");
-//    	label1.setStyleName("label2");
-//    	label2.setHeight("30%");
-//    	vartiInside.addComponents(label1,label2,label3,label4,label5,label6,label7,label8);
-//    	HoriBackgroundLeft.addComponent(vartiInside);
-//    	
-//    	final VerticalLayout vertiInsideHoriRight = new VerticalLayout();
-//    	vertiInsideHoriRight.setWidth("80%");
-//    	vertiInsideHoriRight.setSpacing(true);
-//    	vertiInsideHoriRight.setMargin(true);
-//    	final Label labelR1 = new Label("text 1");
-//    	final Label labelR2 = new Label("text 2");
-//    	final Label labelR3= new Label("text 3");
-//    	final Label labelR4 = new Label("text 4");
-//    	final Label labelR5 = new Label("text 5");
-//    	final Label labelR6 = new Label("text 6");
-//
-//
-//    	vertiInsideHoriRight.addComponents(labelR1,labelR2,labelR3,labelR4,
-//    									   labelR5,labelR6);
-//    	HoriBackgroundLeft.addComponent(vertiInsideHoriRight);
-//    	horlayout.addComponent(HoriBackgroundLeft);
-//    	//detailPanel.setContent(HoriBackgroundLeft);
-//    	final HorizontalLayout horiButton = new HorizontalLayout();
-//    	Button buttonOk = new Button("ok");
-//    	Button buttonCancel = new Button("cancel");
-//    	
-//    	horiButton.addComponents(buttonOk,buttonCancel);
-//    	horiButton.setSpacing(true);
-//    	vertiInsideHoriRight.addComponent(horiButton);
-    	
-    	final Label dateLabel = new Label("Date : 2017-06 ");
-    	dateLabel.setWidth("100%");
-    	dateLabel.setStyleName("dateLabel");
-    	root.addComponent(dateLabel);
-
+    	final HorizontalLayout footerBackground = new HorizontalLayout();
+    	footerBackground.setWidth("100%");
+    	final Label date = new Label("2017-06");
+    	date.setWidth("100%");
+    	date.setStyleName("date");
+    	footerBackground.addComponent(date);
+    	root.addComponent(footerBackground);
+    	root.setComponentAlignment(footerBackground, Alignment.BOTTOM_CENTER);
     }
 
     @WebServlet(urlPatterns = "/*", name = "CpUIServlet", asyncSupported = true)
