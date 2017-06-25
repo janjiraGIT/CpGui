50c2abc0d33bb3d69751de623ed6673c2f22c212
janjira <janjira.eriksson@gmail.com>
2017-06-25 16:36:53 +0200

    Organization the code

--- CpGui/src/main/java/com/mobilityguard/acc/CpGui/CpUI.java
+++ CpGui/src/main/java/com/mobilityguard/acc/CpGui/CpUI.java
@@ -9,6 +9,8 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.GridLayout.OutOfBoundsException;
+import com.vaadin.ui.GridLayout.OverlapsException;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.ListSelect;
@@ -30,76 +32,100 @@ public class CpUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-    	final VerticalLayout root = new VerticalLayout();
-    	root.setStyleName("root");
-    	root.setSizeFull();
-    	setContent(root);
+    	final VerticalLayout rootLayout = new VerticalLayout();
+    	rootLayout.setStyleName("root");
+    	rootLayout.setSizeFull();
+    	setContent(rootLayout);
+    	final HorizontalLayout titleLayout = setHeader();
+    	rootLayout.addComponent(titleLayout);  	
+    	final HorizontalLayout selectLayout = setMenu(rootLayout); 	
+    	setGrid(selectLayout);
+    	setButtonOKandCancel(rootLayout);
+    	setFooter(rootLayout);
+    }
+	/**
+	 * @param rootLayout
+	 */
+	private void setFooter(final VerticalLayout rootLayout) {
+		final HorizontalLayout footerBackground = new HorizontalLayout();
+    	footerBackground.setWidth("100%");
+    	final Label date = new Label("2017-06");
+    	date.setWidth("100%");
+    	date.setStyleName("date");
+    	footerBackground.addComponent(date);
+    	rootLayout.addComponent(footerBackground);
+    	rootLayout.setComponentAlignment(footerBackground, Alignment.BOTTOM_CENTER);
+	}
+	/**
+	 * @param rootLayout
+	 */
+	private void setButtonOKandCancel(final VerticalLayout rootLayout) {
+		final HorizontalLayout buttonLayout= new HorizontalLayout();
+    	buttonLayout.setStyleName("buttonBackground");    	
+    	final Button bOk = new Button("ok");
+    	final Button bCancel = new Button("cancel");  	
+    	buttonLayout.addComponents(bOk,bCancel);
+    	rootLayout.addComponent(buttonLayout);
+    	rootLayout.setComponentAlignment(buttonLayout, Alignment.BOTTOM_RIGHT);
+	}
+	/**
+	 * @param selectBackground
+	 * @throws OverlapsException
+	 * @throws OutOfBoundsException
+	 */
+	private void setGrid(final HorizontalLayout selectLayout) throws OverlapsException, OutOfBoundsException {
+		final GridLayout grid = new GridLayout(6,6);
+    	grid.addStyleName("grid");
+    	grid.setWidth("1000px");
+    	grid.setHeight("400px");
+    	grid.addComponent(new Label("Name : "),1,1);
+    	grid.addComponent(new Label("Surname : "),1,2);
+    	grid.addComponent(new Label("Email : "),1,3);
+    	grid.addComponent(new Label("Address : "),1,4);
+    	grid.addComponent(new Label("Phone :"),1,5);
+    	grid.addComponent(new TextField(),2,1);
+    	grid.addComponent(new TextField(),2,2);
+    	grid.addComponent(new TextField(),2,3);
+    	grid.addComponent(new TextField(),2,4);
+    	grid.addComponent(new TextField(),2,5);
+    	selectLayout.addComponent(grid);
+	}
 
-    	final HorizontalLayout titleBar = new HorizontalLayout();
-    	titleBar.setStyleName("titleBar");
-    	titleBar.setWidth("100%");
-    	root.addComponent(titleBar);   	
-    	final Label titleLabel = new Label("Control Panel");
-    	titleLabel.addStyleName("titleLabel");
-    	titleLabel.setWidth("100%");
-    	titleBar.addComponent(titleLabel);
-
-    	final HorizontalLayout horlayout = new HorizontalLayout();
-    	horlayout.setSizeFull();
-    	root.addComponent(horlayout);
-    	root.setExpandRatio(horlayout, 1);
+	/**
+	 * @param rootLayout
+	 * @return
+	 */
+	private HorizontalLayout setMenu(final VerticalLayout rootLayout) {
+		final HorizontalLayout menuLayout = new HorizontalLayout();
+    	menuLayout.setSizeFull();
+    	rootLayout.addComponent(menuLayout);
+    	rootLayout.setExpandRatio(menuLayout, 1);
     	
-    	final HorizontalLayout selectBackground = new HorizontalLayout();
-    	selectBackground.setStyleName("selectBackground");
-    	horlayout.addComponent(selectBackground);
-    	selectBackground.setWidth("30%");
-    	selectBackground.setHeight("100%");
-    	
+    	final HorizontalLayout selectLayout = new HorizontalLayout();
+    	selectLayout.setStyleName("selectLayout");
+    	menuLayout.addComponent(selectLayout);
+    	selectLayout.setWidth("30%");
+    	selectLayout.setHeight("100%");
     	
     	final ListSelect<String> selectMenu = new ListSelect<>();
     	selectMenu.setStyleName("selectMenu");
     	selectMenu.setItems("Status","Network","TLSS","Syslog");
     	selectMenu.setHeight("100%");
     	selectMenu.setWidth("100%");
-    	selectBackground.addComponent(selectMenu);
-    	
-    	final GridLayout grid = new GridLayout(6,6);
-    	grid.addStyleName("grid");
-    	grid.setWidth("1000px");
-    	grid.setHeight("500px");
-    	
-    	grid.addComponent(new Label("a"),1,0);
-    	grid.addComponent(new Label("b"),1,1);
-    	grid.addComponent(new Label("c"),1,2);
-    	grid.addComponent(new Label("d"),1,3);
-    	grid.addComponent(new Label("e"),1,4);
-    	grid.addComponent(new Label("f"),1,5);
-    	
-    	grid.addComponent(new Label("1"),2,0);
-    	grid.addComponent(new Label("2"),2,1);
-    	grid.addComponent(new Label("3"),2,2);
-    	grid.addComponent(new Label("4"),2,3);
-    	grid.addComponent(new Label("5"),2,4);
-    	grid.addComponent(new Label("6"),2,5);
-
-    	selectBackground.addComponent(grid);
-    	final HorizontalLayout buttonBackground= new HorizontalLayout();
-    	buttonBackground.setStyleName("buttonBackground");    	
-    	final Button bOk = new Button("ok");
-    	final Button bCancel = new Button("cancel");  	
-    	buttonBackground.addComponents(bOk,bCancel);
-    	root.addComponent(buttonBackground);
-    	root.setComponentAlignment(buttonBackground, Alignment.BOTTOM_RIGHT);
-    	
-    	final HorizontalLayout footerBackground = new HorizontalLayout();
-    	footerBackground.setWidth("100%");
-    	final Label date = new Label("2017-06");
-    	date.setWidth("100%");
-    	date.setStyleName("date");
-    	footerBackground.addComponent(date);
-    	root.addComponent(footerBackground);
-    	root.setComponentAlignment(footerBackground, Alignment.BOTTOM_CENTER);
-    }
+    	selectLayout.addComponent(selectMenu);
+		return selectLayout;
+	}
+	private HorizontalLayout setHeader(){
+    	final HorizontalLayout titleLayout = new HorizontalLayout();
+    	titleLayout.setStyleName("titleLayout");
+    	titleLayout.setWidth("100%");
+  	
+    	final Label titleLabel = new Label("Control Panel");
+    	titleLabel.addStyleName("titleLabel");
+    	titleLabel.setWidth("100%");
+    	titleLayout.addComponent(titleLabel);
+		return titleLayout;	
+	}
 
     @WebServlet(urlPatterns = "/*", name = "CpUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = CpUI.class, productionMode = false)
