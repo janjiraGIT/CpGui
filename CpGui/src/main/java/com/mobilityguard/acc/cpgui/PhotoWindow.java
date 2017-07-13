a120c5b018da117f61a804d1a84cd455b6a65a67
janjira <janjira.eriksson@gmail.com>
2017-07-13 22:46:46 +0200

    Image is work

--- CpGui/src/main/java/com/mobilityguard/acc/cpgui/PhotoWindow.java
+++ CpGui/src/main/java/com/mobilityguard/acc/cpgui/PhotoWindow.java
@@ -1,5 +1,6 @@
 package com.mobilityguard.acc.cpgui;
 
+import com.vaadin.server.ClassResource;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.Image;
 import com.vaadin.ui.Panel;
@@ -11,19 +12,22 @@ public class PhotoWindow {
 	public Window createPhotoTheme(){
 		final Window nwWindow = new Window("");
         final VerticalLayout layout = new VerticalLayout();
-
         layout.setSizeFull();
         nwWindow.setContent(layout);
         nwWindow.setPositionX(300);
         nwWindow.setPositionY(65);
         nwWindow.setHeight("80%");
-        nwWindow.setWidth("45%");      
+        nwWindow.setWidth("70%");      
         final Panel panel = new Panel();
         layout.addComponent(panel);
+        layout.addComponent(new Image(null,new ClassResource("images.jpeg")));
 		final ThemeResource resource = new ThemeResource("images.jpeg");
 		final Image image = new Image("Image",resource);
 		image.setSizeFull();
 		panel.setContent(image);
+		panel.setWidth("300px");
+		panel.setHeight("200px");
+		
 		return nwWindow;		
 	}
 }
